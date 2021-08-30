d766a6244a64eb5f04c792394f3508f0c805d3f2
ashura_44056 <sengouy0@gmail.com>
2021-08-30 17:57:25 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,12 @@ import path from "path";
     console.log("Database is initialized");
   });
   if (process.env.NODE_ENV === "production") {
+    console.log("--- Production ---");
     app.use(express.static("web/build"));
     app.get("*", (_req, res) => {
       res.sendFile(path.resolve(__dirname, "web", "build", "index.html"));
     });
   }
-
   const PORT = process.env.PORT || 8000;
   app.listen(PORT, () => {
     console.log("Server is running");
