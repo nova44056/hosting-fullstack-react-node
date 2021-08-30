1e1b0647e68081e833dfbe59dae9d45160177a27
ashura_44056 <sengouy0@gmail.com>
2021-08-30 18:07:48 +0700

    added ormconfig.json

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import { createConnection } from "typeorm";
 import dotenv from "dotenv";
-import { ormconfig } from "./ormconfig";
 import path from "path";
 
 (() => {
@@ -11,7 +10,7 @@ import path from "path";
   dotenv.config();
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  createConnection(ormconfig).then(() => {
+  createConnection().then(() => {
     console.log("Database is initialized");
   });
   if (process.env.NODE_ENV === "production") {
